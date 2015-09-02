9ad7c5e4a015f2178c828bc807091710b78b4168
Travis <tscholl2@uw.edu>
2015-09-02 15:10:18 -0700

    initial commit

---
+++ group.go
@@ -0,0 +1,45 @@
+package group
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"math/big"
+)
+
+type Element []byte
+
+var n = new(big.Int)
+var block cipher.Block
+var G Element
+
+func init() {
+	n.Rsh(big.NewInt(1), 255)
+	block, _ = aes.NewCipher([]byte("a joke should really fit in here"))
+	G = encode(big.NewInt(1))
+}
+
+func ScaleGen(k *big.Int) Element {
+	return Scale(G, k)
+}
+
+func Scale(c Element, k *big.Int) Element {
+	a := decode(c)
+	return encode(new(big.Int).Mul(a, k))
+}
+
+func encode(a *big.Int) (c Element) {
+	block.Encrypt(c, pad(a.Bytes()))
+	return
+}
+
+func decode(c Element) *big.Int {
+	block.Decrypt(c, c)
+	return new(big.Int).SetBytes(c)
+}
+
+func pad(b []byte) []byte {
+	if len(b) <= block.BlockSize() {
+		return append(make([]byte, block.BlockSize()-len(b)), b...)
+	}
+	return append(make([]byte, block.BlockSize()-len(b)%block.BlockSize()), b...)
+}
